5cd3fa5b64ae41394c713de3b3d9821fe1d4a8d4
MarchynskaN <119426243+MarchynskaN@users.noreply.github.com>
2023-07-31 05:05:28 -0700

    first commit

---
+++ Server/Middleware/AuthMiddleware.js
@@ -0,0 +1,41 @@
+//Purpose: user authentication and authorization
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../Models/UserModel.js";
+
+//The 'protect' middleware is used to authenticate users and fetch their information from the database
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+//The 'admin' middleware is used to check if the user is an admin
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an Admin");
+  }
+};
+export { protect, admin };
